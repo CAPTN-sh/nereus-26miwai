badfd95e1bceb31ec1c171c44e8e339076ba061f
Ben Biesenbach <ben.biesenbach@proton.me>
2025-06-02 18:36:46 +0200

    desire with denmark data

---
+++ desire-pytorch-master/desire/nn/rnn_encoder.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from desire.utils import EncoderRNNParams
+
+
+class EncoderRNN(nn.Module):
+    '''Source :
+    https://pytorch.org/tutorials/beginner/chatbot_tutorial.html This will
+    be used in the front end.
+
+    Removed the embedding layer as we it is directly encoded into
+    positions (x,y)
+    '''
+
+    def __init__(self, params: EncoderRNNParams):
+        # hidden_size, n_layers=20, dropout=0
+        super(EncoderRNN, self).__init__()
+        self.params = params
+        self.n_layers = params.n_layers
+        self.dropout = params.dropout
+        self.intermediate_size = params.intermediate_size
+        self.gru_hidden_size = params.gru_hidden_size
+        self.input_size = params.input_size
+        self.kernel_size = params.kernel_size
+
+        self.in_conv1d = nn.Conv1d(self.input_size,
+                                   self.intermediate_size,
+                                   self.kernel_size,
+                                   padding=self.kernel_size // 2)
+
+
+        self.gru = nn.GRU(self.intermediate_size,
+                          self.gru_hidden_size,
+                          self.n_layers,
+                          dropout=(0 if self.n_layers == 1 else self.dropout),
+                          bidirectional=False,
+                          batch_first=True)
+
+    def forward(self, input_seq, hidden=None):
+        packed = F.relu(self.in_conv1d(input_seq)).clone()
+        packed.transpose_(2, 1)
+        # print("Shape of packed", packed.size())
+        # Forward pass through GRU
+        outputs, hidden = self.gru(packed, hidden)
+        return outputs, hidden
+
+
+if __name__ == "__main__":
+    ''' Sample parameters and simple forwar pass test'''
+    sample_parameters = EncoderRNNParams(input_size=2,
+                                         intermediate_size=16,
+                                         gru_hidden_size=48,
+                                         kernel_size=3,
+                                         n_layers=20,
+                                         dropout=0)
+    #
+    a = torch.rand(3, 2, 20)
+    b = EncoderRNN(sample_parameters)
+    o, h = b(a)
---
+++ desire-pytorch-master/desire/utils/params.py
@@ -0,0 +1,137 @@
+from typing import NamedTuple
+import torch.nn as nn
+from dataclasses import dataclass, field
+from typing import List
+import numpy as np
+
+NUM_AGENTS = 16
+NUM_LENGTH = 40
+
+class Exp(nn.Module):
+    def __init__(self):
+        super(Exp, self).__init__()
+
+    def forward(self, x):
+        return x.exp() * 0.5
+
+
+@dataclass
+class EncoderRNNParams:
+    input_size: int = 2
+    intermediate_size: int = 16
+    gru_hidden_size: int = 48
+    kernel_size: int = 3
+    n_layers: int = 40
+    dropout: float = 0.0
+
+
+@dataclass
+class DecoderRNNParams:
+    gru_hidden_size: int = 48
+    output_size: int = 48
+    n_layers: int = 12
+    dropout: float = 0.0
+
+@dataclass
+class CVAEEncoderParams:
+    cond_size: int = 48
+    latent_size: int = 48
+
+    def __post_init__(self):
+        self.layer_sizes = [(self.cond_size + self.latent_size, 48),
+                       (48, 48),
+                       (48, 48)]
+        self.layer_activations = [nn.ReLU(), None, Exp()]
+
+
+
+@dataclass
+class CVAEDecoderParams:
+    cond_size: int = 48
+    latent_size: int = 48
+
+    def __post_init__(self):
+        self.layer_sizes = [(self.cond_size + self.latent_size, 48),
+                            (48, 48)]
+        self.layer_activations = [None, nn.Softmax(dim=1)]
+
+
+@dataclass
+class CVAEParams:
+    cond_size: int = 48
+    latent_size: int = 48
+
+    def __post_init__(self):
+        self.encoder_params = CVAEEncoderParams(cond_size=self.cond_size,
+                                                latent_size=self.latent_size)
+        self.decoder_params = CVAEDecoderParams(cond_size=self.cond_size,
+                                                latent_size=self.latent_size)
+
+
+
+@dataclass
+class SocialPoolingParams:
+    num_rings: int = 6
+    num_wedges: int = 6
+    hidden_size: int = 48
+    num_agents: int = 4
+    rmin: int = 1
+    rmax: int = 400
+
+    def __post_init__(self):
+        self.fc_config = [self.num_rings * self.num_wedges * self.hidden_size,
+                          self.hidden_size,
+                          nn.ReLU()]
+        self.rmax_by_rmin = np.log(int(self.rmax / float(self.rmin)))
+
+@dataclass
+class PoolingCNNParams:
+    layer_configuration: List = field(default_factory = lambda : [(3, 16, (5, 5), 2, nn.Conv2d, nn.ReLU()),
+                                                                  (16, 32, (5, 5), 1, nn.Conv2d, nn.ReLU()),
+                                                                  (32, 32, (5, 5), 1, nn.Conv2d, nn.ReLU())])
+
+@dataclass
+class SCFParams:
+    scene_size: tuple = field(default_factory=lambda: (720, 576, 2))
+    input_size: int = 2
+    intermediate_size: int = 16
+    sp_params: SocialPoolingParams = field(default_factory= lambda: SocialPoolingParams())
+
+    def __post_init__(self):
+        self.velocity_fc = [(self.input_size,
+                             self.intermediate_size,
+                             nn.ReLU())]
+
+
+@dataclass
+class SGMParams:
+    final_output_size: int = 2
+    n_layers_x: int = 8
+    n_layers_y: int = 12
+    n_layers_ypred: int = 12
+
+    cvae_params: CVAEParams = field(default_factory=CVAEParams)
+
+    def __post_init__(self):
+        self.rnn_enc_x_params = EncoderRNNParams(n_layers=self.n_layers_x)
+        self.rnn_enc_y_params = EncoderRNNParams(n_layers=self.n_layers_y)
+        self.rnn_dec_params = DecoderRNNParams(n_layers=self.n_layers_ypred)
+
+
+@dataclass
+class IOCParams:
+    scene_size: tuple = field(default_factory=lambda: (720, 576, 2))
+    num_layers: int = 12
+    num_agents: int = 4
+    num_dims: int = 2
+    gru_params: dict = field(default_factory=lambda: {'input_size': 96,
+                                                      'hidden_size': 48,
+                                                      'num_layers': 1,
+                                                      'dropout': 0,
+                                                      'bidirectional': False,
+                                                      'batch_first': True})
+
+    scoring_fc: list = field(default_factory=lambda: [(48, 1, nn.ReLU())])
+
+    def __post_init__(self):
+        self.scf_params = SCFParams(scene_size=self.scene_size)
