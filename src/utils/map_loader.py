9d323a9d02ae2660c91dfa06ea59862709891ce5
Ben Biesenbach <ben.biesenbach@proton.me>
2025-07-22 15:52:03 +0200

    doc: comments form MapLoader

--- src/utils/map_loader.py
+++ src/utils/map_loader.py
@@ -17,6 +17,9 @@ class MapLoader:
         return cls._instance
 
     def _initialize(self):
+        """
+        Load every map and dissolve them by layer.
+        """
         map_folder = Config().folder["maps"]
         map_files = [
             gpd.read_file(path).to_crs(DEFAULT_CRS)
@@ -26,15 +29,52 @@ class MapLoader:
         self._map_layers = self._maps.dissolve(by="layer")
 
     def get_features(self, layers=[], crs=DEFAULT_CRS) -> gpd.GeoDataFrame:
+        """
+        Returns all the polygons and there names for the given layer.
+
+        Args:
+            layers (List[str]): List of layers to return the named polygons from.
+            crs (str): The CRS to convert to befor returning.
+        Returns:
+            gpd.GeoDataFrame: With index "name" and columns "layer" and "geometry".
+        """
         if layers:
             return self._maps.to_crs(crs)[self._maps["layer"].isin(layers)]
         return self._maps
 
     def get_layers(self, crs=DEFAULT_CRS) -> gpd.GeoDataFrame:
+        """
+        Returns the dissolved polygon of each layer.
+
+        Args:
+            crs (str): The CRS to convert to befor returning.
+
+        Returns:
+            gpd.GeoDataFrame: With index "layer" and column "geometry".
+        """
         return self._map_layers.to_crs(crs)
 
     def get_layer(self, layer, crs=DEFAULT_CRS) -> gpd.GeoDataFrame:
+        """
+        Returns the dissolved polygon the given layer.
+
+        Args:
+            layer (str): The layer of the map.
+            crs (str): The CRS to convert to befor returning.
+
+        Returns:
+            gpd.GeoDataFrame: With index "layer" and column "geometry".
+        """
         return self._map_layers.to_crs(crs).loc[layer]
 
     def total_bounds(self, crs=DEFAULT_CRS):
+        """
+        Returns the total_bounds of the map.
+
+        Args:
+            crs (str): The CRS to convert to befor returning.
+
+        Returns:
+            numpy.ndarray: Bounds in form [min_x, min_y, max_x, max_y]
+        """
         return self._map_layers.to_crs(crs).total_bounds
