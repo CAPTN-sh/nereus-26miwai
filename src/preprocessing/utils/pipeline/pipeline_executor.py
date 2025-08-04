45b5fe029267a8e137a351f1a22a612a630f362b
Ben Biesenbach <ben.biesenbach@proton.me>
2025-08-04 14:25:44 +0200

    try without deque

--- src/preprocessing/utils/pipeline/pipeline_executor.py
+++ src/preprocessing/utils/pipeline/pipeline_executor.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from collections import deque
 from preprocessing.utils.pipeline.pipeline import Pipeline
 
@@ -15,6 +15,22 @@ class PipelineExecutor:
         self.pending = deque()
 
     def run_parallel(self, max_workers=None):
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+            futures = [
+                executor.submit(self.pipeline.execut_task, key, *args)
+                for key, args in self.task_iter
+            ]
+            for future in tqdm(as_completed(futures), total=self.total):
+                try:
+                    result = future.result()
+                    if result is not None:
+                        self.results.append(result)
+                except Exception as err:
+                    print(f"Error processing: {err}")
+
+        return self.pipeline.save_results(self.results)
+
+    def run_parallel2(self, max_workers=None):
         with self.pipeline.init_pool(max_workers) as executor:
             max_inflight = (max_workers or 8) * 8
             self.submit_task(executor, max_inflight)
